613916f4e4aa099db18feadc355455533378a2a3
alvinyanson <a.yanson@arcanys.com>
2025-03-18 10:37:47 +0800

    feat: cart page

--- src/routes/cart/index.tsx
+++ src/routes/cart/index.tsx
@@ -1,9 +1,45 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/cart/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/cart/"!</div>
+  return (
+    <>
+      <h3>Shopping Cart</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <br />
+      <button className="btn btn-dark">Checkout</button>
+    </>
+  );
 }
