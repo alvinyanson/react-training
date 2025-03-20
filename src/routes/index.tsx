a86c694b9906d2fbbb1e1f136026d41e033950e0
alvinyanson <a.yanson@arcanys.com>
2025-03-20 16:49:04 +0800

    feat: added pagination

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,44 @@
-import { createFileRoute } from '@tanstack/react-router';
-import { useState } from 'react';
+import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { fetchMovies } from '../api/movie.service';
 import MovieCard from '../components/movie-card';
 import { Movie } from '../types/movie';
+import { useState } from 'react';
+
+type ProductSearch = {
+  page: number;
+};
 
 export const Route = createFileRoute('/')({
   component: Index,
-  loader: () => fetchMovies(),
+  validateSearch: (search: Record<string, unknown>): ProductSearch => {
+    // validate and parse the search params into a typed state
+    return {
+      page: Number(search?.page ?? 1),
+    };
+  },
+  loaderDeps: ({ search: { page } }) => ({ page }),
+  loader: async ({ deps: { page } }) => {
+    return await fetchMovies(page);
+  },
 });
 
 function Index() {
   const response = Route.useLoaderData();
+  const navigate = useNavigate();
   const [page, setPage] = useState(1);
 
   const handleNextPage = () => {
     setPage(page + 1);
 
+    navigate({ search: { page: page + 1 } });
   };
 
   const handlePrevPage = () => {
     if (page === 1) return;
 
     setPage(page - 1);
+    navigate({ search: { page: page - 1 } });
+
   };
 
   return (
@@ -29,7 +46,7 @@ function Index() {
       <div className="d-flex justify-content-between">
         <h3>Hello!</h3>
         <div className="btn-group" role="group">
-          {/* <button
+          <button
             type="button"
             onClick={handlePrevPage}
             className="btn btn-dark"
@@ -42,7 +59,7 @@ function Index() {
             className="btn btn-dark"
           >
             Next
-          </button> */}
+          </button>
         </div>
       </div>
       <div className="row mt-4">
