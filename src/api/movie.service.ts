e496f43b13770cc58c23b9bb90071b962927ccd2
alvinyanson <a.yanson@arcanys.com>
2025-03-20 16:57:26 +0800

    note on api

--- src/api/movie.service.ts
+++ src/api/movie.service.ts
@@ -6,7 +6,7 @@ export const FALLBACK_IMAGE = 'https://placehold.co/282x158.png';
 
 const BASE_URL = 'https://api.themoviedb.org/3/movie/popular';
 
-const KEY = 'a74e8d76782da6fe2b3d1cf5ebfdb505';
+const KEY = 'a74e8d76782da6fe2b3d1cf5ebfdb505'; // for demo purposes only
 
 export const fetchMovies = async (page: number = 1): Promise<MovieResponse> => {
   const res = await fetch(`${BASE_URL}?api_key=${KEY}&page=${page}`);
