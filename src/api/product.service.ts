6a0a37167c632d1a09e6eb51455e66fcef544f0f
alvinyanson <a.yanson@arcanys.com>
2025-04-02 14:55:43 +0800

    refactor: added env config

--- src/api/product.service.ts
+++ src/api/product.service.ts
@@ -1,15 +1,15 @@
 import { ProductResponse, ProductsResponse } from '@/types/product'
 
-const BASE_URL = 'https://dummyjson.com'
+const baseUrl = import.meta.env.VITE_DUMMY_JSON_BASE_URL
 
 export const fetchProducts = async (): Promise<ProductsResponse> => {
-  const res = await fetch(`${BASE_URL}/products`)
+  const res = await fetch(`${baseUrl}/products`)
   if (!res.ok) throw new Error('Failed to fetch products')
   return res.json()
 }
 
 export const fetchProductById = async (id: number): Promise<ProductResponse> => {
-  const res = await fetch(`${BASE_URL}/products/${id}`)
+  const res = await fetch(`${baseUrl}/products/${id}`)
   if (!res.ok) throw new Error(`Failed to fetch product with id ${id}`)
   return res.json()
 }
