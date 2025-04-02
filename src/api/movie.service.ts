075d385e50f0c76a25afa98a0cc08851ccd80dd0
alvinyanson <a.yanson@arcanys.com>
2025-04-02 15:00:04 +0800

    refactor: added env config

--- src/api/movie.service.ts
+++ src/api/movie.service.ts
@@ -1,15 +1,15 @@
 import { MovieResponse } from '@/type/movie'
 
-export const IMAGE_CDN = 'https://image.tmdb.org/t/p/w300'
+export const IMAGE_CDN = import.meta.env.VITE_IMAGE_CDN
 
-export const FALLBACK_IMAGE = 'https://placehold.co/282x158.png'
+export const FALLBACK_IMAGE = import.meta.env.VITE_FALLBACK_IMAGE
 
-const BASE_URL = 'https://api.themoviedb.org/3/movie/popular'
+const BASE_URL = import.meta.env.VITE_MOVIE_DB_URL
 
-const KEY = 'a74e8d76782da6fe2b3d1cf5ebfdb505' // for demo purposes only
+const KEY = import.meta.env.VITE_KEY // for demo purposes only
 
 export const fetchMovies = async (page: number = 1): Promise<MovieResponse> => {
-  const res = await fetch(`${BASE_URL}?api_key=${KEY}&page=${page}`)
+  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${KEY}&page=${page}`)
   if (!res.ok) throw new Error('Failed to fetch products')
   return res.json()
 }
